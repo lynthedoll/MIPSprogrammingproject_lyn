d90413dbab82906dc5cae363f54c6a149b8d9280
lynthedoll <caitlyn.lynch@bison.howard.edu>
2024-02-15 14:26:09 -0500

    Added loop to process characters in substring: loads address of current character, loads current character, and checks if it is a null terminator.

--- sum_strings.asm
+++ sum_strings.asm
@@ -29,14 +29,14 @@ main:
 # subroutine which processes the input string
 process_whole_string:
     # initialize variables
-    li $t0, 0       # Counter for substring index
-    li $t1, 0       # Counter for sum
+    li $t0, 0       # counter for substring index
+    li $t1, 0       # counter for sum
 
 process_substring:
-    add $t2, $a0, $t0 # load current substring address
-    lb $t3, 0($t2) # load current character
-    beq $t3, $zero, end_process_substring # checks if current character is null terminator
-    beq $t3, delimiter, calculate_sum # checks if current character is delimiter
+    add $t2, $a0, $t0	# load current substring address
+    lb $t3, 0($t2)	# load current character
+    beq $t3, $zero, end_process_substring	# checks if current character is null terminator
+    beq $t3, delimiter, calculate_sum		# checks if current character is delimiter
 
     # increment substring index
     addi $t0, $t0, 1
@@ -48,3 +48,9 @@ calculate_sum:
     li $t5, 10      # multiplier for decimal places, starting at 1 (units)
     li $t6, 1       # flag to indicate positive number (1) or negative number (-1)
     li $t7, 0       # flag to indicate if a number is being formed (0 for no, 1 for yes)
+
+calculate_sum_loop:
+    add $t2, $a0, $t0	# loads address of current character
+    lb $t3, 0($t2)	# loads current character
+    beq $t3, $zero, process_current_number	# checks if current character is null terminator
+
