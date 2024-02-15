f3a4c5f7c6bb8270d50ec567192bc462ca7e1729
lynthedoll <caitlyn.lynch@bison.howard.edu>
2024-02-15 13:38:51 -0500

    Reserved space for input, set '/' as delimiter, and labeled sum. Also, added initial main subroutine to prompt user input

--- sum_strings.asm
+++ sum_strings.asm
@@ -2,3 +2,12 @@
 input_string:   .space 1001   # reserves space for input string (up to 1000 characters + null terminator)
 delimiter:      .asciiz "/"   # defines delimiter as "/"
 sum_label:      .asciiz "Sum: "  # outputs label for sum
+
+    .text
+    .globl main
+
+main:
+    # Prompt user for input string
+    li $v0, 4
+    la $a0, input_prompt
+    syscall
